181e249cb803149a025aa4b698270b9dfff00048
SkaymanT <SkaymanT@yandex.ru>
2020-11-25 14:21:09 +0300

    feat: update page

--- src/main/js/card/card-modal.js
+++ src/main/js/card/card-modal.js
@@ -1,13 +1,13 @@
-import { Modal } from '../modal';
+import { Modal } from "../modal";
 
 export class CardModal extends Modal {
   constructor(
     classes,
-    { id, date, demo, img, description, category, code, stack }
+    { id, time, demo, img, description, category, code, stack }
   ) {
     super(classes);
     this.id = id;
-    this.date = date;
+    this.time = time;
     this.demo = demo;
     this.img = img;
     this.description = description;
@@ -17,58 +17,60 @@ export class CardModal extends Modal {
   }
 
   generateContentCardModal() {
-    this.content = super.createDomNode(this.modal, 'div', 'card-modal-content');
-    this.body = super.createDomNode(this.modal, 'div', 'card-modal-body');
+    this.content = super.createDomNode(this.modal, "div", "card-modal-content");
+    this.body = super.createDomNode(this.modal, "div", "card-modal-body");
     this.imgCardModal = super.createDomNode(
       this.imgCardModal,
-      'img',
-      'card-modal-img'
+      "img",
+      "card-modal-img"
     );
     this.imgCardModal.src = this.img;
     this.imgCardModal.alt = this.id;
 
     this.infoCardModal = super.createDomNode(
       this.infoCardModal,
-      'div',
-      'card-modal-info'
+      "div",
+      "card-modal-info"
     );
 
-    this.infoTitle = super.createDomNode(this.infoTitle, 'h3', 'info-title');
+    this.infoTitle = super.createDomNode(this.infoTitle, "h3", "info-title");
     this.infoTitle.innerHTML = this.id;
 
     this.buttonsModal = super.createDomNode(
       this.buttonsModal,
-      'div',
-      'buttons_modal'
+      "div",
+      "buttons_modal"
     );
 
-    this.infoDemo = super.createDomNode(this.infoDemo, 'a', 'button_primary');
+    this.infoDemo = super.createDomNode(this.infoDemo, "a", "button_primary");
     this.infoDemo.innerHTML = `LIVE DEMO`;
     this.infoDemo.href = this.demo;
 
-    this.infoCode = super.createDomNode(this.infoCode, 'a', 'button_primary');
+    this.infoCode = super.createDomNode(this.infoCode, "a", "button_primary");
     this.infoCode.innerHTML = `CODE`;
     this.infoCode.href = this.code;
 
     this.infoParagraph = super.createDomNode(
       this.infoParagraph,
-      'h5',
-      'info-paragraph'
+      "h5",
+      "info-paragraph"
     );
     this.infoParagraph.innerHTML = this.description;
 
-    this.infoList = super.createDomNode(this.infoList, 'ul', 'info-list');
-    this.infoList.innerHTML = `<li><h5>Date: <span>${this.date}</span></h5></li>
+    this.infoList = super.createDomNode(this.infoList, "ul", "info-list");
+    this.infoList.innerHTML = `<li><h5>Development time: <span>${
+      this.time
+    }</span></h5></li>
     <li><h5>Category: <span>${this.category}</span></h5></li>
-    <li><h5>Tech stack: <span>${this.stack.join(', ')}</span></h5></li>`;
+    <li><h5>Tech stack: <span>${this.stack.join(", ")}</span></h5></li>`;
     this.appendCardModalElement();
     return this.content;
   }
 
   closeModal() {
     super.closeModal();
-    document.querySelectorAll('.card').forEach((element) => {
-      element.classList.remove('active-card');
+    document.querySelectorAll(".card").forEach((element) => {
+      element.classList.remove("active-card");
     });
   }
 
