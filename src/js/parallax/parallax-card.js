924e46aa922cb3859fe15006465dc090c059c001
Sam-Shpakov <skaymant@yandex.ru>
2021-05-04 17:20:42 +0300

    feat: for safari

--- src/js/parallax/parallax-card.js
+++ src/js/parallax/parallax-card.js
@@ -14,13 +14,15 @@ function transformElement(elementOne, elementTwo) {
   } else if (navigator.userAgent.indexOf("YaBrowser") != -1) {
     prefix = "-webkit-";
   } else if (navigator.userAgent.indexOf("Safari") != -1) {
-    prefix = "-webkit-";
+    prefix = "";
   } else if (navigator.userAgent.indexOf("Firefox") != -1) {
-    prefix = "-webkit-";
+    prefix = "-moz-";
   } else {
     prefix = "-webkit-";
   }
-  console.log(prefix);
+  if (prefix === "") {
+    return;
+  }
   return function (mouseEvent) {
     let mouseX, mouseY;
     let transformAmount = 10;
