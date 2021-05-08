774adc612c704abefab7848637b3aec3323f71a9
SkaymanT <SkaymanT@yandex.ru>
2021-05-08 21:26:31 +0300

    feat: rename portfolios to portfolio

--- src/js/card/index.js
+++ src/js/card/index.js
@@ -1,17 +1,17 @@
-import { giveInfoItems } from "../common";
-import { CardModal } from "./card-modal";
-import { generateCards } from "./add-card";
+import { giveInfoItems } from '../common';
+import { CardModal } from './card-modal';
+import { generateCards } from './add-card';
 
 export async function addCardClickHandler() {
   let items = await giveInfoItems();
   document
-    .querySelector(".filter-container")
-    .addEventListener("click", (event) => {
-      if (event.target.closest(".card")) {
+    .querySelector('.filter-container')
+    .addEventListener('click', (event) => {
+      if (event.target.closest('.card')) {
         generateCardModal(
-          getClickedData(items, event.target.closest(".card").id)
+          getClickedData(items, event.target.closest('.card').id)
         );
-        event.target.closest(".card").classList.add("active-card");
+        event.target.closest('.card').classList.add('active-card');
       }
     });
 }
@@ -20,7 +20,7 @@ function getClickedData(items, id) {
   return items.find((item) => item.id.toLowerCase() === id.toLowerCase());
 }
 const generateCardModal = (data) => {
-  let cardModal = new CardModal("modal", data);
+  let cardModal = new CardModal('modal', data);
   cardModal.renderCardModal();
   setTimeout(() => {
     cardModal.openModal();
@@ -29,8 +29,8 @@ const generateCardModal = (data) => {
 
 export async function addCard() {
   const items = await giveInfoItems();
-  let containerCard = document.querySelector(".filter-container");
+  let containerCard = document.querySelector('.filter-container');
   containerCard.remove();
-  let portfolios = document.querySelector(".portfolios__wrapper");
-  portfolios.append(generateCards(items));
+  let portfolio = document.querySelector('.portfolio__wrapper');
+  portfolio.append(generateCards(items));
 }
